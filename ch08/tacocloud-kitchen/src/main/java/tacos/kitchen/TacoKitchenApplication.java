e4532e8c591c2773ce2c6d0030ca7e6f682c836e
JussV <joana.veljanoska@gmail.com>
2019-01-24 23:19:39 +0100

    RabbitMQ pull example

--- ch08/tacocloud-kitchen/src/main/java/tacos/kitchen/TacoKitchenApplication.java
+++ ch08/tacocloud-kitchen/src/main/java/tacos/kitchen/TacoKitchenApplication.java
@@ -2,6 +2,8 @@ package tacos.kitchen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TacoKitchenApplication {
@@ -9,5 +11,13 @@ public class TacoKitchenApplication {
   public static void main(String[] args) {
     SpringApplication.run(TacoKitchenApplication.class, args);
   }
+
+  static final String queueName = "tacocloud.order.queue";
+
+  @Bean
+  Queue queue() {
+    return new Queue(queueName, false);
+  }
+
 
 }
